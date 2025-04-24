1b6858b5152af33dd1d92e58e9e53a70665b540a
bdch <de.zac007@gmail.com>
2025-04-24 12:39:25 +0200

    Removed intertwined branches

---
+++ unit_3/playground_3.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Lingling on 20.04.2025.
+//
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << setw(5) << 2 << '\n';
+    cout << setw(5) << 3 << '\n';
+    cout << setw(5) << 4 << '\n';
+    std::cout.fill('_');
+}
