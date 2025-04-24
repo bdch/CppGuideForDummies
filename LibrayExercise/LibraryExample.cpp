8105dfa34777a37aed046d8b8aae975dbcfa6ffb
bdch <de.zac007@gmail.com>
2025-04-24 11:51:50 +0200

    what git tree even am I?

---
+++ LibrayExercise/LibraryExample.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Lingling on 20.04.2025.
+//
+
+#include "LibraryExample.h"
---
+++ LibrayExercise/LibraryExample.h
@@ -0,0 +1,24 @@
+//
+// Created by Lingling on 20.04.2025.
+//
+
+#ifndef LIBRARYEXAMPLE_H
+#define LIBRARYEXAMPLE_H
+#include<string>
+using namespace std;
+
+
+class LibraryExample
+{
+private:
+    string signature;
+    string author;
+    string title;
+    char type;
+    int playDuration;
+    int pageCount;
+
+};
+
+
+#endif //LIBRARYEXAMPLE_H
